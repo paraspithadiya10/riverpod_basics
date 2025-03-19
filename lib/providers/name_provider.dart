ac7f343f322908673479d1bbb8cea083e4956f29
Paras Pithadiya <paraspithadiya10@gmail.com>
2025-03-19 12:58:07 +0530

    add generated provider for name

---
+++ lib/providers/name_provider.dart
@@ -0,0 +1,7 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'name_provider.g.dart';
+
+@riverpod
+String name(ref) {
+  return 'david warner';
+}
---
+++ lib/providers/name_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'name_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$nameHash() => r'a981f9115976a16fb9545a4e33d68d961ad60420';
+
+/// See also [name].
+@ProviderFor(name)
+final nameProvider = AutoDisposeProvider<String>.internal(
+  name,
+  name: r'nameProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$nameHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef NameRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
